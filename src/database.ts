9724d8fcdc3434249220c4dabd0eec754f887052
Rafael Luz <rafaellladm@gmail.com>
2023-10-21 01:33:03 -0300

    feat: create user database

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { Knex, knex as setupKnex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: './db/app.db',
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
