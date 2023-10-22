06e9d9e69bbcac843ce085b99cbc8df3a13f16fc
Rafael Luz <rafaellladm@gmail.com>
2023-10-21 23:06:58 -0300

    feat: creation of the user route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,23 @@
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
+import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/', () => {
-    return 'Hello World'
+  app.post('/', async (request, reply) => {
+    const createUsersBodySchema = z.object({
+      name: z.string(),
+      email: z.string(),
+    })
+
+    const { name, email } = createUsersBodySchema.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+    })
+
+    return reply.status(201).send()
   })
 }
