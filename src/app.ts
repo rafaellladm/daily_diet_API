a827c0e4c48caa11325bf3ac5ab53bd16ae8649a
Rafael Luz <rafaellladm@gmail.com>
2023-10-27 15:39:23 -0300

    feat: eating with the meals route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
+import { mealsRoutes } from './routes/meals'
 import { usersRoutes } from './routes/users'
 
 export const app = fastify()
 
 app.register(usersRoutes, {
   prefix: 'users',
+})
+
+app.register(mealsRoutes, {
+  prefix: 'meals',
 })
