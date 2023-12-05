eb400e5a65974723fa1b17b042d327ea5ea923bd
Rafael Luz <rafaellladm@gmail.com>
2023-12-04 22:58:30 -0300

    feat: adding password for user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
@@ -8,14 +9,19 @@ export async function usersRoutes(app: FastifyInstance) {
     const createUsersBodySchema = z.object({
       name: z.string(),
       email: z.string(),
+      password: z.string(),
     })
 
-    const { name, email } = createUsersBodySchema.parse(request.body)
+    const { name, email, password } = createUsersBodySchema.parse(request.body)
+
+    // Cadastro de usuário
+    const passwordHash = await hash(password, 8)
 
     await knex('users').insert({
       id: randomUUID(),
       name,
       email,
+      password: passwordHash,
     })
 
     return reply.status(201).send()
