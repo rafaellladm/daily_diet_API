72901979b45f9c9b5aad3c0d51b11440215a0107
Rafael Luz <rafaellladm@gmail.com>
2023-12-10 20:01:11 -0300

    feat: cookie insert

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { mealsRoutes } from './routes/meals'
 import { usersRoutes } from './routes/users'
 
 export const app = fastify()
+
+app.register(cookie)
 
 app.register(usersRoutes, {
   prefix: 'users',
