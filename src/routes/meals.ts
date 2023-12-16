fb6de854663ee5066c48d6bc91dc6cd0b7aa317c
Rafael Luz <rafaellladm@gmail.com>
2023-12-15 23:19:15 -0300

    feat: addition of meal update

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -52,4 +52,50 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(201).send()
     },
   )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const getMealsParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const putMealsParamsSchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        dateBr: z.string(),
+        hour: z.string(),
+        diet: z.boolean(),
+      })
+
+      const { name, description, dateBr, hour, diet } =
+        putMealsParamsSchema.parse(request.body)
+
+      const date = dateBr.split('/')
+
+      const dateTime = new Date(`${date[2]}-${date[1]}-${date[0]} ${hour}`)
+
+      const { id } = getMealsParamsSchema.parse(request.params)
+
+      const meal = await knex('meals').where('id', id).first().update({
+        name,
+        description,
+        date: dateTime.toISOString(),
+        diet,
+      })
+
+      if (!meal) {
+        return reply.status(401).send({
+          error: 'Meal not found.',
+        })
+      }
+
+      return reply.status(200).send({
+        message: 'meal updated successfully!',
+      })
+    },
+  )
 }
